68b53ac32f55573858858de5ef616c0463670670
bruc3balo <brucelosis12@gmail.com>
2022-05-31 05:44:03 +0300

    db

--- src/database/database.js
+++ src/database/database.js
@@ -1,2 +1,2 @@
 const mongoose = require('mongoose');
-module.exports = mongoose.connect('mongodb://127.0.0.1:27017/discrodauth?retryWrites=false');
+module.exports = mongoose.connect(process.env.DATABASE_URL);
